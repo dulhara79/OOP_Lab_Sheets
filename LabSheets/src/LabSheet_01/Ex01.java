f94dcdda8c218fc0d75f6c7ce5052c2aa5052330
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-03-02 12:27:08 +0530

    LabSheetEx01

---
+++ LabSheets/src/LabSheet_01/Ex01.java
@@ -0,0 +1,8 @@
+package LabSheet_01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("It's been nice knowing you.\nGoodbye World!");
+	}
+}
