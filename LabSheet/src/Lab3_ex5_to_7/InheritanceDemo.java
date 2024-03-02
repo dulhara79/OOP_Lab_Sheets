3d3db42f0e6fa742c6b4698e79046173f67238d3
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-03-03 01:47:51 +0530

    Labsheet3-Ex5-7

---
+++ LabSheet/src/Lab3_ex5_to_7/InheritanceDemo.java
@@ -0,0 +1,15 @@
+package Lab3_ex5_to_7;
+
+public class InheritanceDemo {
+	public static void main(String[] args) {
+		Person p1 = new Person("Dulhara", "Colombo");
+		
+		Student s1 = new Student("Kaushalya", "Gampaha", "IT004");
+		
+		PartTimeStudent ps1 = new PartTimeStudent("Noname", "Thumpane", "IT005", 3);
+		
+		p1.showDetails();
+		s1.showDetails();
+		ps1.showDetails();
+	}
+}
