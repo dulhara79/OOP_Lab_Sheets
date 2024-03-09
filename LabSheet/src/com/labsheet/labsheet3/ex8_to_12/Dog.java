187eb26918bec1f7cc522c3a3629a397940ad67f
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-03-10 02:27:41 +0530

    labsheet3_ex8_to_12

---
+++ LabSheet/src/com/labsheet/labsheet3/ex8_to_12/Dog.java
@@ -0,0 +1,20 @@
+package com.labsheet.labsheet3.ex8_to_12;
+
+public class Dog extends Pet {
+	private int noOfMasters;
+	
+	public Dog(String n, String o, int a, int m){
+		super(n, o, a);
+		this.noOfMasters = m;
+	}
+	
+	public Dog(String n, String o) {
+		super(n, o, 0);
+		this.noOfMasters = 1;
+	}
+	
+	public void showDetails() {
+		super.showDetails();
+		System.out.println("I am a dog. I have " + this.noOfMasters + " masters at home.");
+	}
+}
