863c1b04006f2a01fb7fdae6693044277be69874
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-03-10 02:22:47 +0530

    Ex03.java

---
+++ LabSheet/src/com/labsheet/labsheet1/Ex03.java
@@ -0,0 +1,13 @@
+package com.labsheet.labsheet1;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		int age = 18;
+		
+		if(age <= 18) {
+			System.out.println("Child");
+		} else {
+			System.out.println("Adult");
+		}
+	}
+}
