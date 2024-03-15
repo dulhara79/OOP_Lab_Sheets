baeda2d40ddd673ce0e432c941e909cb3da390d3
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-03-15 11:01:32 +0530

    Main.java

--- LabSheet/src/com/labsheet/labsheet5/ex3/Main.java
+++ LabSheet/src/com/labsheet/labsheet5/ex3/Main.java
@@ -1,6 +1,6 @@
 package com.labsheet.labsheet5.ex3;
 
-//import java.util.InputMismatchException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -19,8 +19,7 @@ public class Main {
 		// (a) Input valid integers to the inputs
 		// (b) ArithmeticException division by zero
 		// (c) ArrayIndexOutOfBounds Exception
-		
-		
+
 		try {
 			System.out.print("Enter a value for maxSubjects: ");
 			maxSubjects = scn.nextInt();
@@ -36,8 +35,14 @@ public class Main {
 			System.out.println("Total marks: " + total);
 			System.out.println("Avarage of marks: " + avg);
 
-		} catch (Exception e) {
-			System.out.println("ERROR!!!" + e);
+		} catch (ArithmeticException e) { // (b) ArithmeticException division by zero
+			System.out.println("ArithmeticException division by zero: " + e);
+		} catch (InputMismatchException e) { // (a) Input valid integers to the inputs
+			System.out.println("Input valid integers to the inputs: " + e);
+		} catch (ArrayIndexOutOfBoundsException e) { // (c) ArrayIndexOutOfBounds Exception
+			System.out.println("ArrayIndexOutOfBounds Exception: " + e);
+		} catch (Exception e) { //To catch another big exceptions
+			System.out.println("ERROR: " + e);
 		}
 
 		finally {
@@ -46,30 +51,7 @@ public class Main {
 
 		System.out.println("The end");
 
-//----------------------------------------------------------------------------------------------
-
-		/*
-		 * try {
-		 * System.out.print("Enter a value for maxSubjects: "); maxSubjects =
-		 * scn.nextInt(); } catch (InputMismatchException e) {
-		 * System.out.println("Please input ALL INTEGERS and Try again!" + e); }
-		 * 
-		 * try { for (int i = 0; i < maxSubjects; i++) {
-		 * System.out.print("Enter marks for subject " + (i + 1) + ": "); marks[i] =
-		 * scn.nextInt(); total += marks[i]; } } catch (ArrayIndexOutOfBoundsException
-		 * e) { System.out.println("Array Index out of bound!!" + e); }
-		 * 
-		 * try { avg = (double) total / maxSubjects; System.out.println("Total marks: "
-		 * + total); System.out.println("Avarage of marks: " + avg); } catch
-		 * (ArithmeticException e) {
-		 * System.out.println("This cause to ArithmeticException division by zero!!" +
-		 * e); }
-		 * 
-		 * finally { System.out.println("This code will be gurrentied to run"); }
-		 * 
-		 * System.out.println("The end");
-		 */
-		
 		scn.close();
+
 	}
 }
