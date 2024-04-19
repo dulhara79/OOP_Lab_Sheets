123870ff5fdec3097010f813afad91fc24cb52a0
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-04-20 04:08:55 +0530

    Create Manager.java

---
+++ LabSheet/src/com/tutorial/tutorial4/ex1/Manager.java
@@ -0,0 +1,17 @@
+package com.tutorial.tutorial4.ex1;
+
+public class Manager extends Employee {
+	
+	public Manager() {
+		super(" ", 0);
+	}
+	
+	public double calculateSalary() {
+		double sal = 0;
+		return sal;
+	}
+	
+	public void displayDetails() {
+		
+	}
+}
