cf77b4b34c02230a2d2178e7e2489b07ed6b0127
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-04-20 03:49:48 +0530

    Create Main.java

---
+++ LabSheet/src/com/tutorial/tutorial3_ex2/Main.java
@@ -0,0 +1,15 @@
+package com.tutorial.tutorial3_ex2;
+
+public class Main {
+	public static void main(String[] args) {
+		Feet mylength = new Feet(5, 6);
+		Feet newlen = new Feet(6, 7);
+//		mylength.add(newlen);
+//		mylength.print("Length: "); // 12’1”
+		
+//		Feet.print(mylength);
+
+		Feet f5 = Feet.addFeet(mylength, newlen, mylength);
+		Feet.print(f5);
+	}
+}
