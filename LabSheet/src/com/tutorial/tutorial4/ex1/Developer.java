bcc036c37f646326a5ea8e502232dfd743204ea5
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-04-20 14:49:37 +0530

    Update Developer.java

--- LabSheet/src/com/tutorial/tutorial4/ex1/Developer.java
+++ LabSheet/src/com/tutorial/tutorial4/ex1/Developer.java
@@ -1,17 +1,23 @@
 package com.tutorial.tutorial4.ex1;
 
 public class Developer extends Employee {
+	private double rate;
+	private double hours;
 	
 	public Developer() {
-		super(" ", 0);
+		super("Teshan", 102);
+		this.hours = 10;
+		this.rate = 5000;
 	}
 	
 	public double calculateSalary() {
-		double sal = 0;
-		return sal;
+		return this.hours * this.rate;
 	}
 	
 	public void displayDetails() {
-		
+		System.out.println("Developer name: " + super.name);
+		System.out.println("Developer ID: " + super.employeeId);
+		System.out.println("Developer worked hours: " + this.hours);
+		System.out.println("Developer's hourly rate: " + this.rate);
 	}
 }
