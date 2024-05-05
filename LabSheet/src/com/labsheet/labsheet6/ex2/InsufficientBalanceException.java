f1bd4a8267eed20422a8275c68ef1b3c2586996b
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-05 22:43:52 +0530

    Create InsufficientBalanceException.java

---
+++ LabSheet/src/com/labsheet/labsheet6/ex2/InsufficientBalanceException.java
@@ -0,0 +1,14 @@
+package com.labsheet.labsheet6.ex2;
+
+public class InsufficientBalanceException extends Exception {
+
+	private double amount;
+
+	public InsufficientBalanceException(double amount) {
+		this.amount = amount;
+	}
+
+	public double getAmount() {
+		return this.amount;
+	}
+}
