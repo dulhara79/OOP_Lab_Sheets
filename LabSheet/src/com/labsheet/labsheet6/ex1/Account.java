b5bf9b026cc3ef0ecce2e6122fd6dcdcbc0dbd12
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-05 22:43:29 +0530

    Update Account.java

--- LabSheet/src/com/labsheet/labsheet6/ex1/Account.java
+++ LabSheet/src/com/labsheet/labsheet6/ex1/Account.java
@@ -1,32 +1,39 @@
 package com.labsheet.labsheet6.ex1;
 
 public class Account {
+
 	private int accNo;
-	private double balance;
-	
-	public Account(int accNo , double balance ) {
+	private double amount;
+
+	public Account() {
+		this.accNo = 00;
+		this.amount = 0.0;
+	}
+
+	public Account(int accNo) {
+
 		this.accNo = accNo;
-		this.balance = balance;
 	}
-	
-	public void displayBalance() {
-		System.out.println("Your account balance is: " + this.balance);
+
+	public void displayDetails() {
+
+		System.out.println("Your account number is: " + this.accNo + "\nYour account balance is: " + this.amount);
 	}
-	
-	public void deposite(double dep) {
-		this.balance += dep; 
+
+	public void deposite(double depAmount) {
+
+		this.amount += depAmount;
 	}
-	
-	public void withdraw(double draw) {
-		this.balance -= draw; 
-		if(this.balance < 0) {
-			//this.balance
+
+	public void withdraw(double withdrawAmount) throws InsufficientBalanceException {
+
+		if (this.amount < withdrawAmount) {
+
+			throw new InsufficientBalanceException(withdrawAmount);
+		} else {
+
+			this.amount -= withdrawAmount;
 		}
 	}
-	
-	
-	public double getAmount() {
-		return this.balance;
-	}
-	 
+
 }
